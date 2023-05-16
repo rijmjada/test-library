f28c32c403699eb7d67cfa9c268fa2475efe38a8
Diego <rijmjada@gmail.com>
2023-05-16 13:45:41 -0300

    first

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const menuLateral = document.querySelector(".menu-lateral");
 
 btnAside.addEventListener('click', () => {
     btnAside.classList.toggle("change-position-btn");
+    btnAside.classList.toggle("animation-btn");
 
     menuLateral.classList.toggle("change-position-menu");
 });
